ae99920fb3c52d50caae2d5d3589cdec617f3384
ExplodingInsanity <explodinginsanity@gmail.com>
2019-01-17 17:46:08 +0200

    Added hover effects

--- indexScript.js
+++ indexScript.js
@@ -10,3 +10,24 @@ document.getElementById("polyUp").setAttribute("points", "0, 0 " + width/2 + " "
 document.getElementById("polyLeft").setAttribute("points", "0, 0 " + width/2 + " " + height/2 + ", 0 ," + height);
 document.getElementById("polyRight").setAttribute("points", width + ", 0 " + width/2 + " " + height/2 + ", " + width + " ," + height);
 document.getElementById("polyDown").setAttribute("points", "0, " + height + " , " + width/2 + " " + height/2 + ", " + width + " , " + height);
+
+var a = document.getElementById("middle").height;
+console.log(a);
+$("#middle").css("top",height/2 - document.getElementById("middle").height/2);
+$("#middle").css("left",width/2 - document.getElementById("middle").width/2);
+
+var before;
+$(".menuButton").hover(
+    function(event) {
+        // The mouse has entered the element, can reference the element via 'this'
+        $(".menubutton").css("visibility","hidden");
+        before = $(this).css("visibility","visible");
+        before = $(this).css("clip-path");
+        $(this).css("clip-path","none");
+    },
+    function (event) {
+        // The mouse has left the element, can reference the element via 'this'
+        $(".menubutton").css("visibility","visible");
+        $(this).css("clip-path", before);
+    }
+ );
