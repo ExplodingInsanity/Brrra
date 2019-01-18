12d57b7a24c37cc8eb8e9cfe859163ef1e1aff22
ExplodingInsanity <explodinginsanity@gmail.com>
2019-01-18 15:26:28 +0200

    made the banner scalable, added home button

--- js/trapScript.js
+++ js/trapScript.js
@@ -1,3 +1,14 @@
 var height = $(document.body).height();
 var width = $(document.body).width();
+
+var logoHeight = 200*width/1900;
+document.getElementById("home").width = logoHeight;
+document.getElementById("home").height = logoHeight;
 $("#home").css("left",width/2 - document.getElementById("home").width/2);
+
+$("#home").hover(function(){
+  this.setAttribute('src','../media/Logo3.png');
+  console.log("Fire");
+  }, function(){
+  this.setAttribute('src','../media/Logo.png');
+});
